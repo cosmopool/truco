3a3551279eed76fa815e29cec63cb4d83a9813bf
Kaio Delphino <dash.zet@gmail.com>
2025-07-01 09:30:23 -0300

    build: add support for macos

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,21 +1,53 @@
 #!/bin/sh
 
-mkdir -p includes
-mkdir -p libs
+OS=$(uname -s)
+INCLUDE_DIR=include
+LIB_DIR=lib
 
-if [ ! -d includes/raylib ]; then
-  # git clone --depth 1 --branch 5.5 https://github.com/raysan5/raylib.git includes/raylib
-  if [ ! -f includes/raylib.tar.gz ]; then
-    curl -L https://github.com/raysan5/raylib/releases/download/5.5/raylib-5.5_linux_amd64.tar.gz --output includes/raylib.tar.gz
+setup_raylib() {
+  echo "-- Installing raylib dependency"
+
+  if [ "$OS" = "Linux" ]; then
+    URL=https://github.com/raysan5/raylib/releases/download/5.5/raylib-5.5_linux_amd64.tar.gz
+  elif [ "$OS" = "Darwin" ]; then
+    URL=https://github.com/raysan5/raylib/releases/download/5.5/raylib-5.5_macos.tar.gz
+  else
+    exit "==>> OS ($OS) not suported yet"
   fi
-  tar -xf includes/raylib.tar.gz || exit
-  mv raylib-5.5_linux_amd64/include includes/raylib
-  mv raylib-5.5_linux_amd64/lib/*.a libs/
-  # cleanup
-  rm includes/raylib.tar.gz
-  rm -rf raylib-5.5_linux_amd64
-fi
 
-# cd includes/raylib/src
-# make PLATFORM=PLATFORM_DESKTOP
-# mv libraylib.a ../../../libs/
+  if [ -d "$INCLUDE_DIR"/raylib ] && [ -f "$LIB_DIR"/*raylib*.a ]; then 
+    echo "-- Already installed, skipping..."
+    return
+  fi
+
+  echo "-- Removing old raylib include and library files"
+  rm -rf "$INCLUDE_DIR"/raylib
+  rm "$LIB_DIR"/*raylib*.a
+
+  echo "-- Fetching from: $URL"
+  # git clone --depth 1 --branch 5.5 https://github.com/raysan5/raylib.git includes/raylib
+  # cd includes/raylib/src
+  # make PLATFORM=PLATFORM_DESKTOP
+  # mv libraylib.a ../../../libs/
+  if [ ! -f "$INCLUDE_DIR"/raylib.tar.gz ]; then
+    curl -L "$URL" --output "$INCLUDE_DIR"/raylib.tar.gz
+  fi
+
+  echo "-- Extracting files"
+  tar -xf "$INCLUDE_DIR"/raylib.tar.gz || exit
+  mv raylib-5.5_linux_amd64/"$INCLUDE_DIR" "$INCLUDE_DIR"/raylib
+  mv raylib-5.5_linux_amd64/"$LIB_DIR"/*.a "$LIB_DIR"/
+
+  echo "-- Cleanup temp files and directories"
+  rm "$INCLUDE_DIR"/raylib.tar.gz
+  rm -rf raylib-5.5_linux_amd64
+}
+
+# -------------------------------------------------------------------
+# MAIN
+# -------------------------------------------------------------------
+
+mkdir -p "$INCLUDE_DIR"
+mkdir -p "$LIB_DIR"
+
+setup_raylib
