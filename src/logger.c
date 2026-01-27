fbe3af0d0e1811e6fef9907447d50c6d8cc07196
Kaio Delphino <dash.zet@gmail.com>
2026-01-27 00:20:30 -0300

    feat(logger): add _log & _logDebug functions

--- src/logger.c
+++ src/logger.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int _log(const char* header, const char *msg) {
+  return printf("--- [%s] %s\n", header, msg);
+}
+
 int _logInfo(char *msg) {
-  return printf("-- %s\n", msg);
+  return _log("INFO", msg);
+}
+
+int _logDebug(char *msg) {
+  return _log("DEBUG", msg);
 }
