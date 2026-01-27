6788bff3591bac156108caa401e654b29dbd7905
Kaio Delphino <dash.zet@gmail.com>
2026-01-27 00:27:35 -0300

    chore: remove unused function

--- src/game.c
+++ src/game.c
@@ -8,16 +8,6 @@
 
 #include <assert.h>
 
-void Game_main(Arena *arena, GameState *prev_state) {
-  _logInfo("Reloading main");
-  assert(arena);
-  GameState *state = {0};
-  if (!state) {
-    state = arena_alloc(arena, 1024 * 1024);
-  }
-  state = prev_state;
-}
-
 void Game_init(Arena *arena, void **game_state) {
   const char *header = "INIT";
   _log(header, "Initializing game module");
